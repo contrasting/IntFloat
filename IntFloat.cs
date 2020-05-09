04a4e8f1e831ef474bafe0b300407e1738143c38
Fred Chen <fredchen1994@outlook.com>
2020-05-09 11:53:08 +0100

    Initial implementation (failing tests)

---
+++ IntFloat.cs
@@ -0,0 +1,45 @@
+﻿namespace IntFloat
+{
+    public struct IntFloat
+    {
+        public const int Scale = 1000;
+        public const float Epsilon = 1f / Scale;
+
+        private int _rawValue;
+        private int _scale;
+
+        public int toFlooredInt => _rawValue / _scale;
+        public float toFloat => _rawValue / (float) _scale;
+
+        public IntFloat(int value)
+        {
+            _rawValue = value;
+            _scale = Scale;
+        }
+
+        public static IntFloat operator +(IntFloat self, IntFloat other)
+        {
+            self._rawValue += other._rawValue;
+            return self;
+        }
+        
+        public static IntFloat operator -(IntFloat self, IntFloat other)
+        {
+            self._rawValue += other._rawValue;
+            return self;
+        }
+
+        public static IntFloat operator *(IntFloat self, IntFloat other)
+        {
+            self._rawValue *= other._rawValue;
+            self._scale *= other._scale;
+            return self;
+        }
+
+        public static IntFloat operator /(IntFloat self, IntFloat other)
+        {
+            self._rawValue /= other.toFlooredInt;
+            return self;
+        }
+    }
+}
---
+++ IntFloatTest.cs
@@ -0,0 +1,71 @@
+﻿using Xunit;
+
+namespace IntFloat
+{
+    public class IntFloatTest
+    {
+        [Fact]
+        public void AdditionTest()
+        {
+            IntFloat two = new IntFloat(2000);
+            IntFloat three = new IntFloat(3500);
+            Assert.Equal(5.5f, (two + three).toFloat);
+        }
+
+        [Fact]
+        public void DivisionTestOne()
+        {
+            IntFloat three = new IntFloat(3000);
+            IntFloat two = new IntFloat(2000);
+            Assert.Equal(1.5f, (three / two).toFloat);
+        }           
+        
+        [Fact]
+        public void DivisionTestTwo()
+        {
+            IntFloat a = new IntFloat(200);
+            IntFloat b = new IntFloat(10000);
+            Assert.Equal(0.02f, (a / b).toFloat);
+        }       
+        
+        [Fact]
+        public void DivisionTestThree()
+        {
+            IntFloat a = new IntFloat(10000);
+            IntFloat b = new IntFloat(200);
+            Assert.Equal(50f, (a / b).toFloat);
+        }
+
+        [Fact]
+        public void DivisionTestFour()
+        {
+            IntFloat a = new IntFloat(212);
+            IntFloat b = new IntFloat(9814);
+            Assert.Equal(0.212f / 9.814f, (a / b).toFloat);
+        }         
+        
+        [Fact]
+        public void DivisionTestFive()
+        {
+            IntFloat a = new IntFloat(12000);
+            IntFloat b = new IntFloat(2001);
+            Assert.Equal(12f / 2.001f, (a / b).toFloat);
+        }       
+
+        [Fact]
+        public void MultiplicationTest()
+        {
+            IntFloat a = new IntFloat(10000);
+            IntFloat b = new IntFloat(200);
+            Assert.Equal(2f, (a * b).toFloat);
+        }
+
+        [Fact]
+        public void MultiplicationTestTwo()
+        {
+            IntFloat a = new IntFloat(36504);
+            IntFloat b = new IntFloat(212);
+            Assert.Equal(36.504f * 0.212f, (a * b).toFloat);
+        }
+    }
+}
